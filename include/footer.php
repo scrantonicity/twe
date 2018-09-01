bfb4b9acf5bebe5d9f8af0530edc642a582f69ca
Becky Jesurum <rjesurum@andrew.cmu.edu>
2018-08-31 21:08:03 -0400

    TESTING SO SORRY

---
+++ include/footer.php
@@ -0,0 +1,42 @@
+  <div class="row footer">
+    <div class="col">
+      <ul>
+        <li>©
+          <script type="text/javascript">
+            var theDate = new Date()
+            document.write(theDate.getFullYear())
+          </script> Tartan Wind Ensemble
+        </li>
+        <li>5000 Forbes Ave.</li>
+        <li>Pittsburgh, PA 15213</li>
+        <li>cmutwe@gmail.com</li>
+      </ul>
+    </div>
+    <div class="col">
+      <ul>
+        <li><a href="contact.html">Contact</a></li>
+        <li><a href="https://calendar.google.com/calendar/embed?src=cmutwe%40gmail.com&ctz=America%2FNew_York">Calendar</a></li>
+        <li><a href="about.html">About</a></li>
+        <li><a href="members.html">Members</a></li>
+      </ul>
+    </div>
+    <div class="col">
+      <ul>
+        <li>Connect with TWE</li>
+        <li>
+          <div class="footer-social">
+            <a href="https://www.facebook.com/tartanwindensemble/" target="_blank">
+              <img src="./assets/fb.png" alt="facebook icon">
+            </a>
+            <a href="https://thebridge.cmu.edu/organization/twe" target="_blank">
+              <img src="./assets/bridge.png" alt="the bridge icon">
+            </a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+  </div>
+</body>
+
+</html>
